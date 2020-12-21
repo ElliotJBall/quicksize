04a076d38b6137ec7ca1f9ec8117fca013b7c0ac
Elliot Ball <elliotjordball@gmail.com>
2020-12-21 17:57:06 +0000

    Initial Commit

---
+++ quicksize/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  quicksize
+//
+//  Created by Elliot Ball on 21/12/2020.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
